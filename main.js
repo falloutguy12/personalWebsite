ce6d91dde39556586a44d2adc73ba0d3b2dd1d7a
Derrick Holt <holtshady@gmail.com>
2023-01-24 17:47:24 -0500

    Slide animation completed up and down. JS built function to switch between images and built function to continuily loop through the animation properly

--- main.js
+++ main.js
@@ -8,7 +8,41 @@ const img5 = document.getElementsByClassName('img5');
 
 const imgArr = [img1, img2, img3, img4, img5];
 
+
+
 function imgRandomSelector(arr) {
-    let i = Math.floor(Math.random() * 4);
-    console.log(i);
+    arr.forEach(x => {
+        x.style.display = 'none'
+    });
+    let i = Math.floor(Math.random() * 5);
+    arr[i].style.display = 'block';
+};
+
+const imgSelectorTimer = () => {
+    let executionCounter = 0;
+    if (executionCounter < 1) {
+        setTimeout(() => {imgRandomSelector(imgArr)}, 3000);
+        executionCounter++;
+    } else if (executionCounter == 1) {
+        setInterval(() => {imgRandomSelector(imgArr)}, 4000);
+    } else {
+        console.log("imgSelectorTimer error")
+    }
+};
+
+window.onload = function() {
+    imgSelectorTimer();
+};
+
+const element = document.querySelector('.fadingImgContainer:before');
+
+function startAnimation() {
+    element.style.animation = `slideDown 1s ease-out 2s  forwards, slideUp 1s linear 4s forwards`;
 }
+
+element.addEventListener('animationend', () => {
+    setTimeout(startAnimation, 0);
+});
+
+startAnimation();
+
